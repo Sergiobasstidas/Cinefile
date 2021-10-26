99cf2f60a9035b41d538020f977e1e3b5f2d9bf6
Sergio <sergio.batidas@gmail.com>
2021-10-26 16:10:14 -0300

    detalles de botones y links

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -24,127 +24,127 @@
 </template>
 
 <script>
-  import { Carousel, Slide } from "vue-carousel";
-  import CarouselCard from "@/components/CarouselCard.vue";
+import { Carousel, Slide } from "vue-carousel";
+import CarouselCard from "@/components/CarouselCard.vue";
 
-  export default {
-    name: "HelloWorld",
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-      model: null,
-      windowWidth: 0,
-      carouselSettings: [
-        [
-          [100, 2],
-          [700, 3],
-          [1024, 4],
-          [1300, 5],
-          [1500, 6],
-        ],
-
-        [
-          [100, 2],
-          [800, 3],
-          [1124, 4],
-          [1400, 5],
-          [1700, 6],
-        ],
+  data: () => ({
+    model: null,
+    windowWidth: 0,
+    carouselSettings: [
+      [
+        [100, 2],
+        [700, 3],
+        [1024, 4],
+        [1300, 5],
+        [1500, 6],
       ],
-    }),
-    components: {
-      Carousel,
-      Slide,
-      CarouselCard,
+
+      [
+        [100, 2],
+        [800, 3],
+        [1124, 4],
+        [1400, 5],
+        [1700, 6],
+      ],
+    ],
+  }),
+  components: {
+    Carousel,
+    Slide,
+    CarouselCard,
+  },
+  props: {
+    movieList: Array,
+    title: String,
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+    // TMDB api key: 21b858443ab2bbdbb90fa7c26e40b421
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.windowWidth = window.innerWidth;
     },
-    props: {
-      movieList: Array,
-      title: String,
-    },
-    created() {
-      window.addEventListener("resize", this.handleResize);
-      this.handleResize();
-      // TMDB api key: 21b858443ab2bbdbb90fa7c26e40b421
-    },
-    destroyed() {
-      window.removeEventListener("resize", this.handleResize);
-    },
-    methods: {
-      handleResize() {
-        this.windowWidth = window.innerWidth;
-      },
-    },
-  };
+  },
+};
 </script>
 <style>
-  .carousel {
-    width: 90%;
-    margin: 0 auto;
-  }
+.carousel {
+  width: 90%;
+  margin: 0 auto;
+}
 
-  .VueCarousel-navigation-button {
-    border-radius: 100px;
-    padding: 10px !important;
-    width: 50px;
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #151f30 !important;
-    color: white !important;
-  }
-  .VueCarousel-navigation-next {
-    transform: translateY(-50%) translateX(120%) !important;
-  }
-  .VueCarousel-navigation-prev {
-    transform: translateY(-50%) translateX(-120%) !important;
-  }
-  .VueCarousel-navigation-button:hover {
-    background-color: #2f80ed !important;
-  }
-  .VueCarousel-navigation-button:focus,
-  .VueCarousel-dot:focus {
-    outline: none !important;
-  }
+.VueCarousel-navigation-button {
+  border-radius: 100px;
+  padding: 10px !important;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #151f30 !important;
+  color: white !important;
+}
+.VueCarousel-navigation-next {
+  transform: translateY(-50%) translateX(120%) !important;
+}
+.VueCarousel-navigation-prev {
+  transform: translateY(-50%) translateX(-120%) !important;
+}
+.VueCarousel-navigation-button:hover {
+  background-color: #2f80ed !important;
+}
+.VueCarousel-navigation-button:focus,
+.VueCarousel-dot:focus {
+  outline: none !important;
+}
 
-  .VueCarousel-pagination {
-    text-align: initial !important;
-  }
-  .VueCarousel-dot-container {
-    margin-top: 0 !important;
-    margin-left: 20px !important;
-  }
-  .VueCarousel-dot {
-    margin-top: 0 !important;
-    background-color: #204b87 !important;
-    width: 12px !important;
-    height: 5px !important;
-    border-radius: 16px !important;
-    transition: all 0.5s ease !important;
-    padding: 8px !important;
-  }
-  .VueCarousel-dot--active {
-    background-color: #2f80ed !important;
-    width: 20px !important;
-  }
-  .slide {
-    display: flex;
-    justify-content: center;
-  }
+.VueCarousel-pagination {
+  text-align: initial !important;
+}
+.VueCarousel-dot-container {
+  margin-top: 0 !important;
+  margin-left: 20px !important;
+}
+.VueCarousel-dot {
+  margin-top: 0 !important;
+  background-color: #204b87 !important;
+  width: 12px !important;
+  height: 5px !important;
+  border-radius: 16px !important;
+  transition: all 0.5s ease !important;
+  padding: 8px !important;
+}
+.VueCarousel-dot--active {
+  background-color: #2f80ed !important;
+  width: 20px !important;
+}
+.slide {
+  display: flex;
+  justify-content: center;
+}
+.movieCard {
+  width: 200px;
+  margin: 20px;
+}
+.carousel_title {
+  width: 88%;
+  margin: 0 auto;
+  margin-bottom: 1em;
+}
+.title_text {
+  font-size: 2.5em;
+}
+@media (max-width: 1110px) {
   .movieCard {
-    width: 200px;
-    margin: 20px;
+    width: 130px;
   }
-  .carousel_title {
-    width: 88%;
-    margin: 0 auto;
-    margin-bottom: 1em;
-  }
-  .title_text {
-    font-size: 2.5em;
-  }
-  @media (max-width: 1110px) {
-    .movieCard {
-      width: 130px;
-    }
-  }
+}
 </style>
